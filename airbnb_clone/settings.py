cfc4475d57311bc3f073a0f25ff58f5ec0a5206a
unknown <bhavsarmanthan@gmail.com>
2020-12-11 16:14:51 +0530

    booking

--- airbnb_clone/settings.py
+++ airbnb_clone/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4yxztkagzy&rdgj9mu9*ai#)b__v(6_$*egg*z4-c2$-hlprf@'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,9 +96,9 @@ WSGI_APPLICATION = 'airbnb_clone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'booking_system',
-        'USER': 'root',
-        'PASSWORD':'',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD':env("DATABASE_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '3306'
     }
